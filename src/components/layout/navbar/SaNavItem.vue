a5280c84c8fb0e5890da8de65e486051a4c17a65
M. Reza Zerehpoosh <ironcladgeek@hotmail.com>
2017-07-10 12:42:46 +0430

    0.15.2 - Added v-on:click directive support to 'SaNavItem' component.

--- src/components/layout/navbar/SaNavItem.vue
+++ src/components/layout/navbar/SaNavItem.vue
@@ -2,17 +2,25 @@
   <!-- 
     Default bulma's nav-item anchor tag for navbar links
    -->
-  <a class="nav-item" :href="to">
+  <a class="nav-item" :href="to" @click="itemClicked()">
     <slot></slot>
   </a> 
 </template>
 
 <script>
   export default {
+    name: 'NavbarItem',
     props: {
       to: {
         default: '#',
         type: String
+      }
+    },
+
+    methods: {
+      // Adds v-on:click directive support to this component
+      itemClicked() {
+        this.$emit('click');
       }
     }
   } 
