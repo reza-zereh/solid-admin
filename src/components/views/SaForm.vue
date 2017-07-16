a8b7fd0511baff884a21a46af87382c14cfccaf2
M. Reza Zerehpoosh <ironcladgeek@hotmail.com>
2017-07-16 15:04:13 +0430

    0.19.1 - Working on 'tabs' component colors...

--- src/components/views/SaForm.vue
+++ src/components/views/SaForm.vue
@@ -1,81 +1,88 @@
 <template>
   <form>
-    <sa-input label="Sa Input" id="sa-input" v-model="content" type="danger"></sa-input>
-    
-    <sa-input label="GInput" id="sa-input" v-model="content" type="info" flow="horizontal"></sa-input>
+    <sa-tabs>
+      <sa-tab name="First pane" :selected="true">
+        <sa-input label="Sa Input" id="sa-input" v-model="content" type="danger"></sa-input>
+        
+        <sa-input label="GInput" id="sa-input" v-model="content" type="info" flow="horizontal"></sa-input>
 
-    <sa-textarea label="Sa Textarea" v-model="text"></sa-textarea>
+        <sa-textarea label="Sa Textarea" v-model="text"></sa-textarea>
 
-    <sa-textarea label="Sa Textarea" v-model="text" type="success" flow="horizontal"></sa-textarea>
+        <sa-textarea label="Sa Textarea" v-model="text" type="success" flow="horizontal"></sa-textarea>
+      </sa-tab>
 
-  
-    <div class="field">
-      <label class="label" for="username">Username</label>
-      <p class="control has-icons-left has-icons-right">
-        <input class="input is-success" type="text" placeholder="Text input" value="bulma" id="username">
-        <span class="icon is-small is-left">
-          <i class="fa fa-user"></i>
-        </span>
-        <span class="icon is-small is-right">
-          <i class="fa fa-check"></i>
-        </span>
-      </p>
-      <p class="help is-success">This username is available</p>
-    </div>
-  
-    <div class="field">
-      <label class="label">Email</label>
-      <p class="control has-icons-left has-icons-right">
-        <input class="input is-danger" type="text" placeholder="Email input" value="hello@">
-        <span class="icon is-small is-left">
-          <i class="fa fa-envelope"></i>
-        </span>
-        <span class="icon is-small is-right">
-          <i class="fa fa-warning"></i>
-        </span>
-      </p>
-      <p class="help is-danger">This email is invalid</p>
-    </div>
-  
-    <div class="field">
-      <label class="label">Subject</label>
-      <p class="control">
-        <span class="select">
-          <select>
-            <option>Select dropdown</option>
-            <option>With options</option>
-          </select>
-        </span>
-      </p>
-    </div>
-  
-    <div class="field">
-      <label class="label">Message</label>
-      <p class="control">
-        <textarea class="textarea" placeholder="Textarea"></textarea>
-      </p>
-    </div>
-  
-    <div class="field">
-      <p class="control">
-        <label class="checkbox">
-          <input type="checkbox"> I agree to the
-          <a href="#">terms and conditions</a>
-        </label>
-      </p>
-    </div>
-  
-    <div class="field">
-      <p class="control">
-        <label class="radio">
-          <input type="radio" name="question"> Yes
-        </label>
-        <label class="radio">
-          <input type="radio" name="question"> No
-        </label>
-      </p>
-    </div>
-  
+      <sa-tab name="2nd Pane">
+        <div class="field">
+          <label class="label" for="username">Username</label>
+          <p class="control has-icons-left has-icons-right">
+            <input class="input is-success" type="text" placeholder="Text input" value="bulma" id="username">
+            <span class="icon is-small is-left">
+              <i class="fa fa-user"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fa fa-check"></i>
+            </span>
+          </p>
+          <p class="help is-success">This username is available</p>
+        </div>
+      
+        <div class="field">
+          <label class="label">Email</label>
+          <p class="control has-icons-left has-icons-right">
+            <input class="input is-danger" type="text" placeholder="Email input" value="hello@">
+            <span class="icon is-small is-left">
+              <i class="fa fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fa fa-warning"></i>
+            </span>
+          </p>
+          <p class="help is-danger">This email is invalid</p>
+        </div>
+      
+        <div class="field">
+          <label class="label">Subject</label>
+          <p class="control">
+            <span class="select">
+              <select>
+                <option>Select dropdown</option>
+                <option>With options</option>
+              </select>
+            </span>
+          </p>
+        </div>
+      
+        <div class="field">
+          <label class="label">Message</label>
+          <p class="control">
+            <textarea class="textarea" placeholder="Textarea"></textarea>
+          </p>
+        </div>
+      
+        <div class="field">
+          <p class="control">
+            <label class="checkbox">
+              <input type="checkbox"> I agree to the
+              <a href="#">terms and conditions</a>
+            </label>
+          </p>
+        </div>
+      
+        <div class="field">
+          <p class="control">
+            <label class="radio">
+              <input type="radio" name="question"> Yes
+            </label>
+            <label class="radio">
+              <input type="radio" name="question"> No
+            </label>
+          </p>
+        </div>
+      </sa-tab>
+
+    </sa-tabs>
+
+    <hr>
     <div class="field is-grouped">
       <p class="control">
         <button class="button is-primary">Submit</button>
@@ -90,11 +97,15 @@
 <script>
 import SaInput from '../features/form/SaInput.vue';
 import SaTextarea from '../features/form/SaTextarea.vue';
+import SaTabs from '../features/tab/SaTabs.vue';
+import SaTab from '../features/tab/SaTabPane.vue';
 
 export default {
   components: {
     'sa-input': SaInput,
-    'sa-textarea': SaTextarea
+    'sa-textarea': SaTextarea,
+    'sa-tabs': SaTabs,
+    'sa-tab': SaTab
   },
 
   data() {
