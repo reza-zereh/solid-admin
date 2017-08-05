755643c554c8a50364ef5f1bf3c0c7bed05491fd
M. Reza Zerehpoosh <ironcladgeek@hotmail.com>
2017-08-05 15:50:47 +0430

    Build 'SaSwitch' component and global mixin for reusability of shared pcs of codes.

---
+++ src/components/features/form/SaSwitch.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="sa-switch-container">
+    <div class="sa-switch-wrapper" :class="{'sa-row-reverse': isRtl}">
+      <label class="checkbox sa-switch">
+        <input type="checkbox">
+        <span class="sa-switch-slider sa-switch-round" :class="typeClass"></span>
+      </label>
+      <span class="sa-switch-content">
+        <slot></slot>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import isRtl from '../../global/mixin/isRtl.js';
+export default {
+  name: 'SwitchCheckbox',
+  mixins: [ isRtl ],
+  props: {
+    type: {
+      type: String,
+      default: ''
+    }
+  },
+
+  computed: {
+   // Generates full class name for bulma types (e.g: is-success)
+    typeClass() {
+      return `is-${this.type}`;
+    }
+
+  }
+}
+</script>
