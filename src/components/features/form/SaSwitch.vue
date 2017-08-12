eeef54d23e4e3a1868bd673d16c4fc4f482b808d
M. Reza Zerehpoosh <ironcladgeek@hotmail.com>
2017-08-12 14:37:33 +0430

    0.26.2 - Implemented 'v-model' directive full support for 'SaSwitch' component.

--- src/components/features/form/SaSwitch.vue
+++ src/components/features/form/SaSwitch.vue
@@ -1,8 +1,22 @@
 <template>
+  <!--
+    Switch style checkbox
+
+    Usage:
+      <sa-switch id="id" v-model="model"
+                 type="success/danger/warning/info"
+                 value="value" 
+                 :disabled="true/false"
+      >
+      </sa-switch>
+  -->
   <div class="sa-switch-container">
     <div class="sa-switch-wrapper" :class="{'sa-row-reverse': isRtl}">
       <label class="checkbox sa-switch">
-        <input type="checkbox" :id="id" :checked="checked" @change="update($event.target.checked)" :disabled="disabled" ref="input">
+        <input type="checkbox" :id="id" 
+               :checked="localChecked" @change="update($event.target.checked)" 
+               :disabled="disabled" :value="value" ref="checkbox"
+        >
         <span class="sa-switch-slider sa-switch-round" :class="typeClass"></span>
       </label>
       <span class="sa-switch-content">
@@ -14,11 +28,12 @@
 
 <script>
 import isRtl from '../../global/mixin/isRtl.js';
+
 export default {
   name: 'SwitchCheckbox',
   mixins: [ isRtl ],
 
-  // Changing v-model default prop and event which are 'value' and 'input'
+  // Changing v-model default prop and event from 'value' and 'input'
   model: {
     prop : 'checked',
     event: 'change'
@@ -33,8 +48,7 @@ export default {
       default: ''
     },
     checked: {
-      type   : Boolean,
-      default: false
+      type   : Boolean
     },
     disabled: {
       type   : Boolean,
@@ -43,6 +57,15 @@ export default {
     value: {
       type: String
     }
+  },
+
+  data: () => ({
+    // local variable to track this checkbox's checked status
+    localChecked: null
+  }),
+
+  mounted() {
+    this.localChecked = this.checked;
   },
 
   computed: {
@@ -54,9 +77,7 @@ export default {
 
   methods: {
     update(value) {
-      // console.log(value);
-      // this.checked = value;
-      // this.$refs.input.checked = value;
+      this.localChecked = value;
       this.$emit('change', value);
     } 
   }
