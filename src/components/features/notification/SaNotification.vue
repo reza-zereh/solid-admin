a019083986612c68c58ead39c9a0410cc3206a99
ironcladgeek <ironcladgeek@hotmail.com>
2017-08-13 18:28:57 +0430

    Resolved 'SaNotification' bug that caused DOMException error which close #8

--- src/components/features/notification/SaNotification.vue
+++ src/components/features/notification/SaNotification.vue
@@ -1,27 +1,27 @@
 <template>
   <!--
-    Notification component.
-
-    Usage:
-      this.$notification.method({props});
-      methods: open, info, error, success, warning
-
-    Props:
-      content  : content to display
-      type     : notification type (primary, success, info, warning, danger)
-      closeIn  : auto close notification in specified seconds. (default: 3)
-      width    : width of notification in percentage
-      showClose: display close button or not
-
-    TODO: Add transition effect
-    TODO: Build documentation
-  -->
-    <div class="notification sa-notification" :class="[typeClass, showClose ? '' : 'p1_25']" :style="widthSize">
-      <button class="delete" @click="close" v-if="showClose"></button>
-      <p>
-        {{ content }}
-      </p>
-    </div>
+        Notification component.
+    
+        Usage:
+          this.$notification.method({props});
+          methods: open, info, error, success, warning
+    
+        Props:
+          content  : content to display
+          type     : notification type (primary, success, info, warning, danger)
+          closeIn  : auto close notification in specified seconds. (default: 3)
+          width    : width of notification in percentage
+          showClose: display close button or not
+    
+        TODO: Add transition effect
+        TODO: Build documentation
+      -->
+  <div class="notification sa-notification" :class="[typeClass, showClose ? '' : 'p1_25']" :style="widthSize">
+    <button class="delete" @click="close" v-if="showClose"></button>
+    <p>
+      {{ content }}
+    </p>
+  </div>
 </template>
 
 <script>
@@ -29,29 +29,30 @@ export default {
   name: 'Notification',
   props: {
     content: {
-      type   : String,
+      type: String,
       default: ''
     },
     type: {
-      type   : String,
+      type: String,
       default: 'primary'
     },
     closeIn: {
-      type   : Number,
+      type: Number,
       default: 3
     },
     width: {
-      type   : Number,
+      type: Number,
       default: null
     },
     showClose: {
-      type   : Boolean,
+      type: Boolean,
       default: true
     }
   },
 
   data: () => ({
-    parentId: 'sa__notification__container'
+    parentId: 'sa__notification__container',
+    isClosed: false
   }),
 
   computed: {
@@ -67,9 +68,12 @@ export default {
   },
 
   methods: {
-    // Remove notification element from DOM
+    // Remove notification element from DOM if this isn't already closed explicitly by 'x' button
     close() {
-      document.getElementById(this.parentId).removeChild(this.$el);
+      if (!this.isClosed) {
+        this.isClosed = true;
+        document.getElementById(this.parentId).removeChild(this.$el);
+      }
     },
 
     // Auto close the notification after the specified seconds
